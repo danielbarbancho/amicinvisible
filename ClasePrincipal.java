ceb306da5ce876f044b0942586dc1c0f6e8857ff
danielbarbancho <73601819+danielbarbancho@users.noreply.github.com>
2020-12-15 16:39:40 +0100

    Add files via upload

---
+++ ClasePrincipal.java
@@ -0,0 +1,36 @@
+
+public class ClasePrincipal {
+
+	public static void main(String[] args) {
+		
+		String arrayPersones [] = {"DanielBarbancho","JoelBerrocal","MarcBouzas","XavierBermuda","NilCarvajal","QuimComas","AleixDeumal","AdrianGil","SergioGarcia","PauRiera","RubenRecolons","GerardFernandez","GerardPedrero","CarlosAbelSilva","OriolPacheco","PauDomenec","PolPavon","JosepRecolons","MartiPuig","MarcelJosepPauli","JoanPardo"};
+		String arrayInvisible [] = new String [21];
+		int numrandom = (int) (21*Math.random()+1);
+		int numrandomas;
+		
+		
+		for(int fila = 0; fila < arrayInvisible.length;fila++) {
+				numrandomas = numrandom;
+				if(arrayPersones[(numrandomas - 1)].equals(" ")) {
+					numrandom = (int) (21*Math.random()+1);
+					numrandomas = numrandom;
+					fila--;
+				} else {
+					arrayInvisible [fila] = arrayPersones [(numrandomas - 1)];
+				arrayPersones[(numrandomas - 1)] = " ";
+			}
+		}
+
+		for(int fila = 0; fila < arrayInvisible.length;fila++) {
+			if(fila == 20) {
+				System.out.print(arrayInvisible[0]);
+			} else {
+			System.out.println(arrayInvisible[fila]);
+			System.out.println("|");
+			System.out.println("V");
+			}
+		}
+	}
+		
+
+}
