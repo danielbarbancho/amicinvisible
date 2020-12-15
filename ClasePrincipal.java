3f5c64801ec9e8c7e742114726f0013a66755a63
danielbarbancho <73601819+danielbarbancho@users.noreply.github.com>
2020-12-15 17:01:25 +0100

    Add files via upload

--- ClasePrincipal.java
+++ ClasePrincipal.java
@@ -3,7 +3,7 @@ public class ClasePrincipal {
 
 	public static void main(String[] args) {
 		
-		String arrayPersones [] = {"DanielBarbancho","JoelBerrocal","MarcBouzas","XavierBermuda","NilCarvajal","QuimComas","AleixDeumal","AdrianGil","SergioGarcia","PauRiera","RubenRecolons","GerardFernandez","GerardPedrero","CarlosAbelSilva","OriolPacheco","PauDomenech","PolPavon","JosepRecolons","MartiPuig","MarcelJosepPauli","JoanPardo"};
+		String arrayPersones [] = {"DanielBarbancho","JoelBerrocal","MarcBouzas","XavierBermuda","NilCarvajal","QuimComas","AleixDeumal","AdrianGil","SergioGarcia","PauRiera","RubenRecolons","GerardFernandez","GerardPedrero","CarlosAbelSilva","OriolPacheco","PauDomenec","PolPavon","JosepRecolons","MartiPuig","MarcelJosepPauli","JoanPardo"};
 		String arrayInvisible [] = new String [21];
 		int numrandom = (int) (21*Math.random()+1);
 		int numrandomas;
@@ -22,7 +22,7 @@ public class ClasePrincipal {
 		}
 
 		for(int fila = 0; fila < arrayInvisible.length;fila++) {
-			if(fila == 21) {
+			if(fila == 20) {
 				System.out.print(arrayInvisible[0]);
 			} else {
 			System.out.println(arrayInvisible[fila]);
